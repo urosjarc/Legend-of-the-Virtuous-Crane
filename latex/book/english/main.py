35b06a5a7c4cdfc97deab5316675395c8d55ae6e
Uroš Jarc <contact@urosjarc.com>
2019-02-11 08:34:05 +0100

    Adding parser.

--- latex/book/english/main.py
+++ latex/book/english/main.py
@@ -1,12 +1,32 @@
 def main():
-	newText = []
-	with open('answer') as file:
+
+	count = 0
+	with open('answer.tex') as file:
 		lines = file.readlines()
+
+		lines = ['\\vspace*{1.2cm}\n'] + lines
+
 		for i, line in enumerate(lines):
 			if line.endswith('medskip\n'):
+				count += 1
+
+				if count % 14 == 0:
+					pagebreak = '\\vfill\\pagebreak\\vspace*{1.2cm}\n'
+
+				else:
+					pagebreak = '\n'
 				txt = line.replace('\\medskip\n', '')
-				lines = '\\textbf{' + txt + '}\\medskip\n'
-				print(lines)
+				lines[i] = '\\textit{' + txt + '}\\medskip' + pagebreak
+
+
+			elif not line.isspace() and not line.startswith('\\'):
+				txt = line.replace('\n', '')
+				lines[i] = '\\textbf{' + txt + '}\n'
+
+
+		with open('answer_new.tex', 'w') as file:
+			file.writelines(lines)
+
 
 
 if __name__ == '__main__':
